2020d0bcb011049661951f9049008f2642548c15
Sharven Prasad Dhanasekar <Sharven.Dhanasekar@ec.gc.ca>
2023-06-08 16:35:37 -0400

    set up RAMP docsite

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,75 @@
+import { version } from '../../package.json';
+
+export default {
+    title: 'R4MP',
+    description: 'Reusable Accessible Mapping Platform 4.0 documentation',
+    lang: 'en-CA',
+    base: `/ramp4-pcar4/{{ramp-version}}/docs/`,
+    srcDir: '../docs',
+    outDir: '../vite-docs',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        logo: '/logo.svg',
+        lastUpdatedText: 'Last Updated',
+        search: {
+            provider: 'local'
+        },
+        socialLinks: [
+            {
+                icon: 'github',
+                link: 'https://github.com/ramp4-pcar4/ramp4-pcar4'
+            }
+        ],
+        nav: [
+            {
+                text: 'Overview',
+                link: 'toc'
+            },
+            {
+                text: 'API Reference',
+                link: `api-tech-docs/index`
+            },
+            {
+                text: `v${version}`,
+                items: [
+                    {
+                        text: 'Releases',
+                        link: 'https://github.com/ramp4-pcar4/ramp4-pcar4/releases'
+                    }
+                ]
+            }
+        ],
+        sidebar: [
+            {
+              text: "Section A",
+              collapsible: true,
+              items: [
+                { text: "Introduction", link: "/introduction" },
+                { text: "Getting Started", link: "/getting-started" },
+              ],
+            },
+            {
+              text: "Section B",
+              collapsible: false,
+              items: [
+                { text: "Introduction", link: "/introduction" },
+                { text: "Getting Started", link: "/getting-started" },
+              ],
+            },
+            {
+              text: "Section C",
+              collapsible: true,
+              items: [
+                { text: "Introduction", link: "/introduction" },
+                { text: "Getting Started", link: "/getting-started" },
+              ],
+            },
+        ],
+        footer: {
+            message: "Released under the MIT License.",
+            copyright: "Copyright © 2021-present Government of Canada",
+        },
+    }
+};
